b4fad4c3eb56ea9c4b67a09f3659c135a3a92e1a
KOHYEONJEONG <080821@naver.com>
2024-07-25 02:09:34 +0900

    h2 database 설정 및 테스트
    Signed-off-by: KOHYEONJEONG <080821@naver.com>

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -7,19 +7,21 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
 	EntityManager em;
 
 	@Test
-	@Transactional
 	void contextLoads() {
 
 		Hello hello = new Hello();
